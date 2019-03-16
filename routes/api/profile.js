d566fab24649d3abb681a425b4573187413f51ea
calebkish <37940332+calebkish@users.noreply.github.com>
2019-03-16 00:09:27 -0400

    Added profile validation and fixed bug in POST request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile Model
 const Profile = require('../../models/Profile');
 // Load User Profile
@@ -20,6 +23,7 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
   const errors = {};
 
   Profile.findOne({ user: req.user.id })
+    .populate('user', ['name', 'avatar'])
     .then(profile => {
       if (!profile) {
         errors.noprofile = 'There is no profile for this user';
@@ -34,6 +38,14 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @desc    Create or Edit user profile
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateProfileInput(req.body);
+
+  // Check Validate
+  if (!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
   // Get fields
   const profileFields = {};
   profileFields.user = req.user.id;
@@ -58,7 +70,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   if (req.body.linkin) profileFields.social.linkin = req.body.linkin;
   if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
-  Profile.findOne({ user: requestAnimationFrame.user.id })
+  Profile.findOne({ user: req.user.id })
     .then(profile => {
       if (profile) {
         // Update
