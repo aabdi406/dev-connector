45fb19948af6c88e4e996b0a2aa236ffd26daf06
calebkish <37940332+calebkish@users.noreply.github.com>
2019-04-03 18:54:06 -0400

    Added Posts component and subcomponents; added related actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';    // combines all our other reducers
 import authReducer from './authReducer';    // authentication state object
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 
 // This is the root reducer which is used to initialize the store
 // This takes in other reducers as input and "reduces" them to a state object
 export default combineReducers({
   auth: authReducer,                        // represents a single reducer brought in, authReducer
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
